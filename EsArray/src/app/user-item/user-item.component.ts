d9f51446b85bfd8c71e407842dca26993dfe6492
Luca <lucmil2000@gmail.com>
2019-03-03 16:48:30 +0000

    ciao

---
+++ EsArray/src/app/user-item/user-item.component.ts
@@ -0,0 +1,23 @@
+
+import {
+  Component,
+  OnInit,
+  Input // <--- aggiungi questo
+ } from '@angular/core';
+
+@Component({
+  selector: 'app-user-item',
+  templateUrl: './user-item.component.html',
+  styleUrls: ['./user-item.component.css']
+})
+
+export class UserItemComponent implements OnInit {
+  @Input() name: string; // <-- Aggiungi la notazione @input
+
+  constructor() {
+     // Rimuovi l'impostazione del nome
+  }
+
+  ngOnInit() {}
+}
+
---
+++ EsVariabili/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { UserItemComponent } from './user-item/user-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserItemComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
