cedf97beca2a01531d34569fe3a00ab7b32b2278
Luca <lucmil2000@gmail.com>
2019-04-15 08:22:05 +0000

    ciao

---
+++ PossibleVerifica/src/app/hero-view/hero-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-view',
+  templateUrl: './hero-view.component.html',
+  styleUrls: ['./hero-view.component.css']
+})
+export class HeroViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ Verifica1/src/app/app.module.ts
@@ -0,0 +1,17 @@
+//https://github.com/Montrasi/AiutoCompari/tree/master/src/app
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
